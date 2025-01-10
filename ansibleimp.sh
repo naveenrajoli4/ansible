778ef42bb781fb27bd513aa2712aef168e940fc4
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-10 09:12:46 +0530

    ansible

--- ansibleimp.sh
+++ ansibleimp.sh
@@ -4,5 +4,11 @@ ssh ec2-user@<IP> 'echo "hello world" > /tmp/hello.txt'
 # -i = inventory --> List of IP address ansible connect to
 ansible all -i <IP>, -e ansible_user=ec2-user -e ansible_password=DevOps321 -m ping
 ansible all -i <IP>, -e ansible_password=DevOps321 -b -m dnf -a "name=naginx state=installed"
-ansible all -i 3.82.122.105, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m dnf -a "name=nginx state=installed"
-ansible all -i 3.82.122.105, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m service -a "name=nginx state=started"
+ansible all -i <IP>, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m dnf -a "name=nginx state=installed"
+ansible all -i <IP>, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m service -a "name=nginx state=started enabled=yes"
+#Below both run at a  time 
+ansible all -i 3.82.122.105, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m dnf -a "name=nginx state=installed" && \
+ansible all -i 3.82.122.105, -e ansible_user=ec2-user -e ansible_password=DevOps321 -b -m service -a "name=nginx state=started enabled=true"
+
+# Below command is to run the playbook in ansible server
+ansible-playbook -i <IP>, -e ansible_user=ec2-user -e ansible_password=DevOps321 nginx_setup.yml
